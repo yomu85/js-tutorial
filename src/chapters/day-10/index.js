01b9406c18c1676c7e9c8ef99782a7365cb3e543
jacob <yomu85@gmail.com>
2025-11-27 18:39:57 +0900

    day-10 메서드 진행 중 2 - 과제 계산기만들기 전까지

--- src/chapters/day-10/index.js
+++ src/chapters/day-10/index.js
@@ -61,3 +61,59 @@ user10.f(); // John  (this == user)
 admin10.f(); // Admin  (this == admin)
 
 admin10['f'](); // Admin (점과 대괄호는 동일하게 동작함)
+
+function sayHi20() {
+  console.log(`sayHi20: ${this}`);
+}
+
+// 객체 없이 호출하기: this == undefined
+sayHi20(); // undefined
+
+// ES6 모듈 (.mjs 또는 type: "module")
+// ES6 모듈은 자동으로 엄격 모드로 실행됩니다
+// "use strict"를 명시하지 않아도 엄격 모드가 적용됨
+
+let user11 = {
+  firstName: "보라",
+  sayHi() {
+    let arrow = () => console.log(this.firstName);
+    arrow();
+  },
+  sayHi2: () => {
+    console.log(this.firstName);
+  }
+}
+
+// 화살표 함수는 자신만의 this를 가지지 않는다는 점에서 독특합니다. 화살표 함수 안에서 this를 사용하면, 외부에서 this 값을 가져옵니다.
+user11.sayHi()
+// user11.sayHi2() // error
+
+function makeUser() {
+  return {
+    name: "John",
+    ref() {
+      return this;
+    }
+  };
+};
+
+let user20 = makeUser();
+
+console.log(user20.ref().name);
+
+// 계산기 만들기 과제
+let calculator = {
+  read() {
+
+  },
+  sum() {
+
+  },
+  mul() {
+
+  }
+}
+
+calculator.read();
+alert(calculator.sum())
+alert(calculator.mul())
