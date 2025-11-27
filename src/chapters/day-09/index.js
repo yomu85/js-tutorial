960f516520b77b71e68b412e251ead820664bcf8
jacob <yomu85@gmail.com>
2025-11-27 16:29:50 +0900

    가비지콜렉션 진행 중 1, index 스타일 수정

--- src/chapters/day-09/index.js
+++ src/chapters/day-09/index.js
@@ -1,1 +1,33 @@
-console.log('시작안함');
+let a = { value: 1 };
+let b = a;  // 두 변수가 같은 객체 참조
+
+// 메모리:
+// a ──┐
+//     └──→ { value: 1 }
+// b ──┘
+a.value = 2;
+a = null;  // a에 null 할당
+
+console.log(a)
+console.log(b)
+
+function marry(man, woman) {
+  woman.husband = man;
+  man.wife = woman;
+
+  return {
+    father: man,
+    mother: woman
+  }
+}
+
+let family = marry({
+  name: "John"
+}, {
+  name: "Ann"
+});
+
+delete family.father;
+delete family.mother.husband;
+
+console.log(family);
