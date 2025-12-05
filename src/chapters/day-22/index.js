7ada73c50c9f112078a15f1dd1742b2feafe7304
jacob <yomu85@gmail.com>
2025-12-05 12:52:31 +0900

    day-22 재귀함수 진행중 1

--- src/chapters/day-22/index.js
+++ src/chapters/day-22/index.js
@@ -1,1 +1,19 @@
-console.log("시작 안 함");
+// function pow(x, n) {
+//   let result = 1;
+
+//   for (let i = 0; i < n; i++) {
+//     result *= x;
+//   }
+
+//   return result;
+// }
+
+function pow(x, n) {
+  if (n === 1) {
+    return x;
+  } else {
+    return x * pow(x, n - 1);
+  }
+}
+
+console.log(pow(2, 3));
