c56701dd453470970a5798e210b0e753eb1f133e
jacob <yomu85@gmail.com>
2025-12-09 11:13:47 +0900

    day-24 변수, 스코프 렉시컬환경까지 진행중 1

--- src/chapters/day-24/index.js
+++ src/chapters/day-24/index.js
@@ -1,1 +1,42 @@
-console.log('시작 안 함');
+(function f() {
+  let message = "Hello";
+  console.log(message);
+})()
+
+function sayHiBye(firstName, lastName) {
+  function getFullName() {
+    return firstName + ' ' + lastName;
+  }
+  console.log("Hello, " + getFullName());
+  console.log("Bye, " + getFullName());
+}
+
+sayHiBye('John', 'Doe');
+
+function makeCounter() {
+  let count = 0;
+  return function() {
+    return count++;
+  }
+}
+
+let counter = makeCounter();
+console.log('counter(): 1번째 시도', counter());
+console.log('counter(): 2번째 시도', counter());
+console.log('counter(): 3번째 시도', counter());
+
+function outer() {
+  let x = 10;
+  
+  function inner() {
+    let y = 20;  // outer는 y를 모름!
+    console.log(x + y);  // x와 y를 찾을 수 있을까?
+  }
+  inner();
+}
+
+outer();
+
+console.log(typeof func1);  // "function" ✅
+console.log(func1());  // func1
+function func1() { return 'func1'; }
