40e6ff67a61432e14412ac4689e1b35c001154d7
jacob <yomu85@gmail.com>
2025-12-15 18:17:58 +0900

    day-30 프로토타입 심화 F.prototype 진행중 1

--- src/chapters/day-30/index.js
+++ src/chapters/day-30/index.js
@@ -1,1 +1,31 @@
-console.log('시작 안 함');
+let animal = {
+  eats: true
+}
+
+function Rabbit(name) {
+  this.name = name;
+}
+
+Rabbit.prototype = animal;
+
+let rabbit = new Rabbit("흰 토끼");
+console.log(rabbit.eats);
+
+function Rabbit2() {
+  // 디폴트 prototype:
+  // Rabbit.prototype = { constructor: Rabbit }
+
+  let rabbit2 = new Rabbit2();
+  console.log(rabbit2.constructor == Rabbit2);
+}
+
+function Rabbit3(name) {
+  this.name = name;
+  console.log(name);
+}
+
+let rabbit3 = new Rabbit3("흰 토끼");
+
+console.log(Rabbit3 === Rabbit3.prototype.constructor);  // false
+
+let rabbit3Clone = new rabbit3.constructor("검은 토끼");
